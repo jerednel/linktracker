7b6a0416aea5c290ab10056776b90ed55e9cad3f
Jeremy Nelson <jerednel@gmail.com>
2014-04-18 10:15:07 -0500

    force encoding of csvs

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,7 +3,7 @@ class Link < ActiveRecord::Base
 	belongs_to :user
 
 		def self.import(file, user_id)
-			CSV.foreach(file.path, headers: true) do |row|
+			CSV.foreach(file.path, headers: true, :encoding => 'ISO-8859-1') do |row|
 				Link.create! row.to_hash.merge(user_id: user_id)
 			end
 		end
