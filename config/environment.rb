9a6ac07ffffe8c765b7f7daa4a42d574bd0f455a
Jeremy Nelson <jerednel@gmail.com>
2014-04-19 22:12:41 -0500

    another

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Linktracker::Application.initialize!
-
-config.action_mailer.default_url_options = { :host => 'linktracker.heroku.com' }
